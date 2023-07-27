54973ed8d0c7c30b5d486476d24d1e703b6bbb76
Ebubekir KARAKURT <114517935+ebubekirkarakurt@users.noreply.github.com>
2023-07-28 02:37:55 +0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const express = require('express')
-
-const app = express()
-const PORT = 4000
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT}`);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  fs.readFile('api/detail.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the file:', err);
+      res.status(500).json({ error: 'Error reading the file' });
+    } else {
+      try {
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+      } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+        res.status(500).json({ error: 'Error parsing JSON' });
+      }
+    }
+  });
+});
 
 app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+  res.send('This is my about route..... ');
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
